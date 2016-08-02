af2a094af4fd904e80ab54172693b94e6c6b2891
maohhgg <maohhgg@gmail.com>
2016-08-02 21:55:55 +0800

    Android Notification

--- OnlineOrOffline/app/src/main/java/com/example/mao/onlineoroffline/LoginActivity.java
+++ OnlineOrOffline/app/src/main/java/com/example/mao/onlineoroffline/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.mao.onlineoroffline;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -61,11 +66,38 @@ public class LoginActivity extends BaseActivity {
                     user.moveToFirst();
                     if(pw.equals(user.getString(user.getColumnIndex("password")))){
                         startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        simpleNotify();
                     } else {
                         Toast.makeText(LoginActivity.this,"密码错误！",Toast.LENGTH_LONG).show();
                     }
                 }
             }
         });
+    }
+    private void simpleNotify(){
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        //Ticker是状态栏显示的提示
+        builder.setTicker("登录成功");
+        //第一行内容  通常作为通知栏标题
+        builder.setContentTitle("状态");
+        //第二行内容 通常是通知正文
+        builder.setContentText("你已经登录了");
+        //第三行内容 通常是内容摘要什么的 在低版本机器上不一定显示
+//        builder.setSubText("这里显示的是通知第三行内容！");
+        //ContentInfo 在通知的右侧 时间的下面 用来展示一些其他信息
+        builder.setContentInfo("2");
+        //number设计用来显示同种通知的数量和ContentInfo的位置一样，如果设置了ContentInfo则number会被隐藏
+        builder.setNumber(1);
+        //可以点击通知栏的删除按钮删除
+        builder.setAutoCancel(true);
+        //系统状态栏显示的小图标
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        //下拉显示的大图标
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.login));
+        //通知默认的声音 震动 呼吸灯
+        builder.setDefaults(NotificationCompat.DEFAULT_LIGHTS);
+        Notification notification = builder.build();
+        manager.notify(1,notification);
     }
 }
--- ReadContentProvider/app/src/main/java/com/example/mao/readcontentprovider/MainActivity.java
+++ ReadContentProvider/app/src/main/java/com/example/mao/readcontentprovider/MainActivity.java
@@ -4,14 +4,10 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
