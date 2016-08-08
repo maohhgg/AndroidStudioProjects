2c4cfdd239bd4b9bdad645e62bd2988e4608e9bc
maohhgg <maohhgg@gmail.com>
2016-08-08 11:33:49 +0800

    Service/AlarmManager  定时任务

--- SMS/app/src/main/java/com/example/mao/sms/MyService.java
+++ SMS/app/src/main/java/com/example/mao/sms/MyService.java
@@ -1,14 +1,39 @@
 package com.example.mao.sms;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.SystemClock;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MyService extends Service {
     private final static String TAG = "MainActivity";
-    private int i = 0;
+    private String i = "";
     private boolean isRun = true;
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder{
+        public void startDown(){
+            Log.i(TAG,"开始下载");
+        }
+
+        public String getProgress(){
+            Log.i(TAG,"当前下载进程" + i);
+            return i;
+        }
+    }
 
     public MyService() {
 
@@ -16,31 +41,77 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.i(TAG,"绑定服务");
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG,"服务创建");
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        //Ticker是状态栏显示的提示
+        //builder.setTicker("登录成功");
+        //第一行内容  通常作为通知栏标题
+        builder.setContentTitle("状态");
+        //第二行内容 通常是通知正文
+        builder.setContentText("后台运行中");
+        //第三行内容 通常是内容摘要什么的 在低版本机器上不一定显示
+        //builder.setSubText("这里显示的是通知第三行内容！");
+        //ContentInfo 在通知的右侧 时间的下面 用来展示一些其他信息
+        //builder.setContentInfo("2");
+        //number设计用来显示同种通知的数量和ContentInfo的位置一样，如果设置了ContentInfo则number会被隐藏
+        //builder.setNumber(1);
+        //可以点击通知栏的删除按钮删除
+        builder.setAutoCancel(true);
+        //系统状态栏显示的小图标
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        //下拉显示的大图标
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        //通知默认的声音 震动 呼吸灯
+        //builder.setDefaults(NotificationCompat.DEFAULT_LIGHTS);
+        Notification notification = builder.build();
+        manager.notify(1,notification);
+        super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG,"Start MyServer");
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (isRun){
+//                    Calendar calendar = Calendar.getInstance();
+//                    Date date = calendar.getTime();
+//                    i = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+//                    Log.i(TAG,"Thread :" + i);
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }).start();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (isRun){
-                    Log.i(TAG,"Thread :" + i++);
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+                Log.i(TAG,"date: " + new Date().toString());
             }
         }).start();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int time = 10 * 1000;
+        long triggerAtTime = SystemClock.elapsedRealtime() + time;
+        Intent i = new Intent(this,AlarmReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(this,0,i,0);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+        Log.i(TAG,"服务销毁");
         super.onDestroy();
         this.isRun = false;
     }
--- SMS/app/src/main/java/com/example/mao/sms/ThreadActivity.java
+++ SMS/app/src/main/java/com/example/mao/sms/ThreadActivity.java
@@ -1,9 +1,11 @@
 package com.example.mao.sms;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,11 +16,32 @@ import android.widget.TextView;
 
 public class ThreadActivity extends AppCompatActivity {
     private TextView tv;
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDown();
+
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("date",downloadBinder.getProgress());
+            msg.setData(bundle);
+            msg.what = 1;
+            handler.sendMessage(msg);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg){
             if(msg.what == 1){
-                tv.setText("Life Is Wonderful");
+                Bundle bundle = msg.getData();
+                tv.setText(bundle.getString("date"));
             }
         }
     };
@@ -39,6 +62,9 @@ public class ThreadActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Message msg = new Message();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("date","HA HA HA HA");
+                        msg.setData(bundle);
                         msg.what = 1;
                         handler.sendMessage(msg);
                     }
@@ -68,8 +94,10 @@ public class ThreadActivity extends AppCompatActivity {
                 stopService(new Intent(this,MyService.class));
                 break;
             case R.id.action_bind_server:
+                bindService(new Intent(this,MyService.class),connection,BIND_AUTO_CREATE);
                 break;
             case R.id.action_unbind_server:
+                unbindService(connection);
                 break;
         }
 
